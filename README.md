e0c40f0169945102929e5cd268caaeeb58815c0f
Manish <manish@parkzap.com>
2019-04-15 15:02:06 +0530

    initial commit to task watchdog timer

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # taskWatchdogTimer
+The ESP-IDF has support for two types of watchdogs: The Interrupt Watchdog Timer and the Task Watchdog Timer (TWDT). The Interrupt Watchdog Timer and the TWDT can both be enabled using make menuconfig, however the TWDT can also be enabled during runtime. The Interrupt Watchdog is responsible for detecting instances where FreeRTOS task switching is blocked for a prolonged period of time. The TWDT is responsible for detecting instances of tasks running without yielding for a prolonged period.
+
+
+# Test:
+change "#define WATCHDOG_TIMEOUT_S 5" to "#define WATCHDOG_TIMEOUT_S 4"
+changing the timeout of 5sec to 4sec
+As Task takes 5sec, if watchdog timeout is 4sec, then system will restart aftertimeout
---
+++ main/main.cpp
@@ -0,0 +1,62 @@
+
+#include "freertos/FreeRTOS.h"
+#include "esp_wifi.h"
+#include "esp_system.h"
+#include "esp_event.h"
+#include "esp_event_loop.h"
+#include "nvs_flash.h"
+#include "driver/gpio.h"
+#include <esp_task_wdt.h>
+#include <stdio.h>
+#include <stdint.h>
+
+extern "C"
+{
+	void app_main(void);
+}
+
+#define WATCHDOG_TIMEOUT_S 5
+
+void myTask(void *myData)
+{
+
+	printf("# Registering our new task with the task watchdog.\n");
+	esp_task_wdt_add(NULL);
+
+	while (1)
+	{
+
+		for (int i = 0; i < 5; i++)
+		{
+			vTaskDelay(1000 / portTICK_PERIOD_MS);
+			printf("Tick :%d\n", i);
+		}
+
+		printf("# Our current task priority is %d.\n", uxTaskPriorityGet(NULL));
+		esp_task_wdt_reset();
+
+		printf("# Removing our watchdog registration before we end the task.\n");
+		esp_task_wdt_delete(NULL);
+	}
+	vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+
+	printf("App starting\n");
+	printf("Initializing the task watchdog subsystem with an interval of 4 seconds.\n");
+
+	// task watchdog timer timeout of 4sec
+	esp_task_wdt_init(WATCHDOG_TIMEOUT_S, true);
+
+	printf("Creatign a new task.\n");
+
+	xTaskCreate(myTask,	// Task code
+				"My Task", // Name of task
+				2048,	  // Stack size
+				NULL,	  // Task data
+				1,		   // Priority
+				NULL	   // task handle
+	);
+}
